3da99dc14284aab4c151ed571359612e36c76344
Alvaro <6666alvaro666@gmail.com>
2020-04-29 18:05:00 -0500

    get input values

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
 // cotizador
+function Seguro(Brand, Year, Type) {
+  this.brand = Brand;
+  this.year = Year;
+  this, (type = Type);
+}
+//control ui
+function Interfase() {}
+//EventListener
+const form = document.getElementById("cotizar-seguro");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-function Seguro() {}
+  //select Brand
+  const brand = document.getElementById("marca");
+  const brandSelected = brand.options[brand.selectedIndex].value;
+  console.log(brandSelected);
 
+  //select Year
+  const year = document.getElementById("anio");
+  const yearSelected = year.options[year.selectedIndex].value;
+  console.log(yearSelected);
+});
+//Year dinamic
 const max = new Date().getFullYear(),
   min = max - 21;
 const selectYears = document.getElementById("anio");
